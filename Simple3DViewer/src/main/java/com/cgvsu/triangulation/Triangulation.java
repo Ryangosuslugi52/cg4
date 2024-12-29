07a3ad868d3bb0fe567f4efaf753e4846bb5e3e5
Cucculcus <dnazarov2005@yandex.ru>
2024-12-29 16:17:18 +0300

    Триангуляция наверное получше

--- Simple3DViewer/src/main/java/com/cgvsu/triangulation/Triangulation.java
+++ Simple3DViewer/src/main/java/com/cgvsu/triangulation/Triangulation.java
@@ -1,53 +1,47 @@
 package com.cgvsu.triangulation;
 
 import com.cgvsu.model.Polygon;
-
 import java.util.ArrayList;
 
 public class Triangulation {
-    public static ArrayList<Polygon> triangulatePolygon (Polygon poly) {
-        int vertexNum = poly.getVertexIndices().size(); // число точек в исходном полигоне
-        ArrayList<Polygon> polygons = new ArrayList<Polygon>(); // список треугольных полигонов
 
-        if (poly.getVertexIndices().size() == 3) { // если передан треугольник, то возвращаем его
-            polygons.add(poly);
-            return polygons;
+    // Триангуляция одного полигона
+    public static ArrayList<Polygon> triangulatePolygon(Polygon poly) {
+        int vertexCount = poly.getVertexIndices().size();
+        ArrayList<Polygon> triangles = new ArrayList<>();
+
+        // Если в полигоне меньше 3 вершин, его невозможно триангулировать
+        if (vertexCount < 3) {
+            return triangles;
         }
 
-        for (int i = 2; i < vertexNum - 1; i++) {
-            ArrayList<Integer> vertex = new ArrayList<>(); // список точек в новом треугольнике
-            vertex.add(poly.getVertexIndices().get(0));
-            vertex.add(poly.getVertexIndices().get(i - 1));
-            vertex.add(poly.getVertexIndices().get(i));
-
-            Polygon currPoly = new Polygon(); //
-            currPoly.setVertexIndices(vertex);
-            polygons.add(currPoly);
+        // Преобразование полигона с 3 вершинами не требуется
+        if (vertexCount == 3) {
+            triangles.add(poly);
+            return triangles;
         }
-        if (vertexNum > 3) { // последний треугольник
-            ArrayList<Integer> vertex = new ArrayList<>();
-            vertex.add(poly.getVertexIndices().get(0));
-            vertex.add(poly.getVertexIndices().get(vertexNum - 2));
-            vertex.add(poly.getVertexIndices().get(vertexNum - 1));
 
-            Polygon currPoly = new Polygon();
-            currPoly.setVertexIndices(vertex);
-            polygons.add(currPoly);
+        // Фан-триангуляция (triangle fan)
+        for (int i = 1; i < vertexCount - 1; i++) {
+            ArrayList<Integer> vertexIndices = new ArrayList<>();
+            vertexIndices.add(poly.getVertexIndices().get(0));   // Центральная вершина
+            vertexIndices.add(poly.getVertexIndices().get(i));   // Текущая вершина
+            vertexIndices.add(poly.getVertexIndices().get(i + 1)); // Следующая вершина
+
+            Polygon triangle = new Polygon();
+            triangle.setVertexIndices(vertexIndices);
+            triangles.add(triangle);
         }
-        return polygons;
+        return triangles;
     }
-    public static ArrayList<Polygon> triangulateModel (ArrayList<Polygon> polygons){
-        ArrayList<Polygon> newModelPoly = new ArrayList<Polygon>();
 
-        for (int i = 0; i < polygons.size(); i++) {
-//            if (polygons.get(i).getVertexIndices().size() < 4) {
-//                newModelPoly.add(polygons.get(i));
-//                continue;
-//            }
-            newModelPoly.addAll(
-                    triangulatePolygon(polygons.get(i))
-            );
+    // Триангуляция списка полигонов
+    public static ArrayList<Polygon> triangulateModel(ArrayList<Polygon> polygons) {
+        ArrayList<Polygon> triangulatedPolygons = new ArrayList<>();
+
+        for (Polygon poly : polygons) {
+            triangulatedPolygons.addAll(triangulatePolygon(poly));
         }
-        return newModelPoly;
+        return triangulatedPolygons;
     }
 }
